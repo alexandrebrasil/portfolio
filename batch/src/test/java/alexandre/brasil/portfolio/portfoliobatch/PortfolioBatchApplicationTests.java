160a33bc7b1662b390ca1106112480433be52a9b
Alexandre Brasil <alexandre.brasil@gmail.com>
2023-03-30 00:16:28 -0300

    Inicialização do projeto batch pelo spring initializer

---
+++ batch/src/test/java/alexandre/brasil/portfolio/portfoliobatch/PortfolioBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package alexandre.brasil.portfolio.portfoliobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
