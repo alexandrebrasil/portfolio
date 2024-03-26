4c7ceb4117e888d73233368fc1147af37dfc4066
Alexandre Brasil <alexandre.brasil@gmail.com>
2024-03-26 20:40:00 -0300

    Cálculo de retorno (total e anual) de cada compra

--- frontend/src/app/ledger/ledger.component.ts
+++ frontend/src/app/ledger/ledger.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 import { TransacaoExtendida } from "../db";
-
+import { xirr, XirrInput, convertRate, RateInterval }  from 'node-irr';
+import * as moment from "moment";
 
 @Component({
     selector: 'portfolio-ledger',
@@ -8,7 +9,7 @@ import { TransacaoExtendida } from "../db";
     styleUrls: [ './ledger.component.scss' ]
 })
 export class LedgerComponent implements OnChanges {
-    colunas = ['data', 'data-ex', 'tipo', 'unitario', 'quantidade', 'valorFinanceiro', 'resultadoFinanceiro', 'resultado', 'acoes'];
+    colunas = ['data', 'data-ex', 'tipo', 'unitario', 'quantidade', 'valorFinanceiro', 'resultadoFinanceiro', 'resultado', 'retornoAnual', 'retorno', 'acoes'];
 
     resultadoFinanceiroAcumulado: number;
     custoLiquido: number;
@@ -17,29 +18,117 @@ export class LedgerComponent implements OnChanges {
 
     hoje = new Date().toISOString().substring(0, 10);
 
+    @Input('preco-mercado')
+    precoMercado: number;
+
     @Input()
-    transacoes: TransacaoExtendida[];
+    set transacoes(transacoes: TransacaoExtendida[]) {
+        this._transacoes = transacoes?.map(t => ({
+            ... t, 
+            possuiRetorno: t.tipo === 'compra',
+            retorno: null,
+            retornoAnual: null,
+            finalizada: null
+        }))
+        
+        this.preencheRetornos(this._transacoes);
+    }
+    
+    _transacoes: TransacaoComRetorno[];
 
     @Output()
     onDeleteTransacao = new EventEmitter<TransacaoExtendida>()
 
     ngOnChanges() {
-        const ultimaTransacao = this.transacoes?.[this.transacoes?.length - 1];
+        const ultimaTransacao = this._transacoes?.[this._transacoes?.length - 1];
 
         this.custoLiquido = -ultimaTransacao?.quantidadeAcumulada * ultimaTransacao?.precoMedioFinanceiro || 0;
         this.quantidadeAtual = ultimaTransacao?.quantidadeAcumulada || 0;
         
         this.resultadoFinanceiroAcumulado = 
-            this.transacoes
+            this._transacoes
                 ?.filter(tx => tx.tipo === 'venda')
                 .map(tx => tx.tipo === 'venda' ? (tx.valorFinanceiro - tx.precoMedioFinanceiro * (tx.quantidade || 0)) : 0)
                 .reduce((r1, r2) => r1 + r2, 0);
 
         this.resultadoAcumulado = 
-            this.transacoes
+            this._transacoes
                 ?.filter(tx => tx.tipo === 'venda')
                 .map(tx => tx.tipo === 'venda' ? (tx.valorContabil - tx.precoMedio * (tx.quantidade || 0)) : 0)
                 .reduce((r1, r2) => r1 + r2, 0);
+    }
+
+    preencheRetornos(transacoes: TransacaoComRetorno[]) {
+        transacoes?.forEach((t, idx) => {
+            if(t.tipo === 'compra') {
+                let quantidade = t.quantidade,
+                    quantidadeAnterior = t.quantidadeAcumulada - t.quantidade,
+                    transacoesIrr: XirrInput[] = [{amount: t.valorFinanceiro, date: t.data}],
+                    dataUltimaVenda;
+
+
+                
+                for(let i = idx + 1; i < transacoes.length && quantidade > 0; i++) {
+                    let transacaoAtual = transacoes[i];
+                    if(transacaoAtual.tipo === "compra") {
+                        continue;
+                    } else if(transacaoAtual.tipo === 'venda') {
+                        let quantidadeVendida = transacaoAtual.quantidade;
+
+                        if(quantidadeAnterior > 0) {
+                            quantidadeAnterior -= quantidadeVendida;
+                            if(quantidadeAnterior >= 0) {
+                                // Quantidade vendida não afetou a compra sendo calculada
+                                quantidadeVendida = 0;
+                            } else {
+                                // Quantidade vendida que afeta esta compra é apenas o que superou a quantidade anterior
+                                quantidadeVendida = -quantidadeAnterior;
+                                quantidadeAnterior = 0;
+                            }
+                        }
+
+                        if(quantidadeVendida > 0) {
+                            if(quantidade < quantidadeVendida) {
+                                quantidadeVendida = quantidade;
+                            }
+                            
+                            let valorFinanceiro = (transacaoAtual.valorFinanceiro / transacaoAtual.quantidade) * quantidadeVendida;
+    
+                            transacoesIrr.push({amount: valorFinanceiro, date: transacaoAtual.data});
+                            quantidade -= quantidadeVendida; 
+                            dataUltimaVenda = moment(t.data);
+                        }
+
+                    } else if(transacaoAtual.tipo === 'desdobramento') {
+                        quantidade *= transacaoAtual.multiplicador;
+                        quantidadeAnterior *= transacaoAtual.multiplicador;
+                    } else if(transacaoAtual.tipo === 'grupamento') {
+                        quantidade /= transacaoAtual.multiplicador;
+                        quantidadeAnterior /= transacaoAtual.multiplicador;
+                    } else if(transacaoAtual.tipo === "bonificação") {
+                        quantidade += quantidade * transacaoAtual.multiplicador / 100;
+                        quantidadeAnterior += quantidadeAnterior * transacaoAtual.multiplicador / 100;
+                    } else if(transacaoAtual.tipo === 'jcp') {
+                        transacoesIrr.push({amount: transacaoAtual.valor * quantidade * 0.85, date: transacaoAtual.data})
+                    } else {
+                        transacoesIrr.push({amount: transacaoAtual.valor * quantidade, date: transacaoAtual.data})
+                    }
+                }
+
+                if(quantidade > 0) {
+                    transacoesIrr.push({amount: quantidade * this.precoMercado, date: moment().format("YYYY-MM-DD")})
+                    dataUltimaVenda = moment()
+                }
+
+                t.finalizada = quantidade <= 0;
+                t.possuiRetorno = true;
+                
+                let irr = xirr(transacoesIrr);
+                t.retornoAnual = convertRate(irr.rate, RateInterval.Year)
+                t.retorno = convertRate(irr.rate, irr.days)
+            }
+        });
+
     }
 
 
@@ -50,4 +139,9 @@ export class LedgerComponent implements OnChanges {
     }
 }
 
-
+type TransacaoComRetorno = TransacaoExtendida & {
+    possuiRetorno: boolean;
+    retorno: number;
+    retornoAnual: number;
+    finalizada: boolean;
+}
